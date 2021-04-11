9e4536dc7c8455991c645443f8cc1fa635cca987
Michael Ramassamy <michaelr@MacBook-Pro-de-Michael.local>
2021-04-11 16:04:53 +0200

    Update font with montserrat

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,9 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 // A custom theme for this app
 const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Montserrat, Roboto, Arial, sans-serif',
+  },
   palette: {
     primary: {
       main: '#00b4e6',
