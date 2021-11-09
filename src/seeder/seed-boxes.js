6c0d671a69b354ee93629135d53eac7764c3bc1d
Ymassamar <ramassamymichael@gmail.com>
2021-11-09 15:15:18 +0100

    Update seed for boxIdeas

--- src/seeder/seed-boxes.js
+++ src/seeder/seed-boxes.js
@@ -1,69 +1,72 @@
 const seedBoxes = [
   {
     "title": "Christmas Box",
-    "category": "Gift",
+    "category": "Themed",
     "description": "Give your employee a beautiful Christmas Box content for well-being with premium products like : 1 box of organic Chocolate, 1 body lotion...",
-    "main_picture_url": "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/Straight%20GF%2FCKHS1.png?alt=media&token=a979c36e-5cec-4aad-8894-72b4bf99cd9a",
-    "picture_urls": ["https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/Straight%20GF%2FCKHS1.png?alt=media&token=a979c36e-5cec-4aad-8894-72b4bf99cd9a", "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/45%20Degree%20GF%2Fckhandsan.png?alt=media&token=7a40d86a-9c89-4dba-9004-ce7eaa32d777"],
+    "main_picture_url": "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/boxIdea_placeholder.png?alt=media&token=b85ed1ec-594c-4384-9e13-c53bc2576de9",
+    "picture_urls": ["https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/boxIdea_placeholder.png?alt=media&token=b85ed1ec-594c-4384-9e13-c53bc2576de9", "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/45%20Degree%20GF%2Fckhandsan.png?alt=media&token=7a40d86a-9c89-4dba-9004-ce7eaa32d777"],
     "tags": ["christmas", "gift", "food"],
     "variants": [
       {
         "name": "Basic",
-        "currency": "£",
-        "price": 9,
         "items": [
-          {"productId": "KX4z2CZxzmUfSbiEsP30", "variantId": "0" , "qty": 2},
-          {"productId": "Z83Ya2tX7yNSUlCvysbD","variantId": "0", "qty": 1},
-          {"productId": "DDK5v1KHzawISSP3ZZqA", "variantId": "0", "qty": 1}
+          {"productId": "KX4z2CZxzmUfSbiEsP30", "variantSKU": "CKHC002-50" , "qty": 2, "productInfos": {} },
+          {"productId": "Z83Ya2tX7yNSUlCvysbD", "variantSKU": "CKPB-MIX3", "qty": 1, "productInfos": {} },
+          {"productId": "DDK5v1KHzawISSP3ZZqA", "variantSKU": "CKACB0013", "qty": 1, "productInfos": {} }
         ],
-        "sku": "B-CH-001-01"
+        "sku": "B-CH-001-01",
+        "boxPrice": null,
+        "minBoxPrice": null,
+        "currency": "£"
       },
       {
         "name": "Premium",
-        "currency": "£",
-        "price": 14,
         "items": [
-          {"productId": "KX4z2CZxzmUfSbiEsP30", "variantId": "0" , "qty": 3},
-          {"productId": "Z83Ya2tX7yNSUlCvysbD","variantId": "0", "qty": 2},
-          {"productId": "DDK5v1KHzawISSP3ZZqA", "variantId": "0", "qty": 1}
+          {"productId": "KX4z2CZxzmUfSbiEsP30", "variantSKU": "CKHC002-50" , "qty": 3, "productInfos": {} },
+          {"productId": "Z83Ya2tX7yNSUlCvysbD", "variantSKU": "CKPB-MIX3", "qty": 2, "productInfos": {} },
+          {"productId": "DDK5v1KHzawISSP3ZZqA", "variantSKU": "CKACB0013", "qty": 1, "productInfos": {} }
         ],
-        "sku": "B-CH-001-02"
+        "sku": "B-CH-001-02",
+        "boxPrice": null,
+        "minBoxPrice": null,
+        "currency": "£"
       }
     ]
   },
   {
     "title": "Best Seller Box",
-    "category": "Gift",
+    "category": "Occasions",
     "description": "Reward your best sales employee with this incredible Box including well-being premium products like : 1 box of organic Chocolate, 1 body lotion...",
-    "main_picture_url": "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/Straight%20GF%2FCKHS1.png?alt=media&token=a979c36e-5cec-4aad-8894-72b4bf99cd9a",
-    "picture_urls": ["https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/Straight%20GF%2FCKHS1.png?alt=media&token=a979c36e-5cec-4aad-8894-72b4bf99cd9a", "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/45%20Degree%20GF%2Fckhandsan.png?alt=media&token=7a40d86a-9c89-4dba-9004-ce7eaa32d777"],
+    "main_picture_url": "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/boxIdea_placeholder.png?alt=media&token=b85ed1ec-594c-4384-9e13-c53bc2576de9",
+    "picture_urls": ["https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/boxIdea_placeholder.png?alt=media&token=b85ed1ec-594c-4384-9e13-c53bc2576de9", "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/45%20Degree%20GF%2Fckhandsan.png?alt=media&token=7a40d86a-9c89-4dba-9004-ce7eaa32d777"],
     "tags": ["reward", "gift", "sales"],
     "variants": [
       {
         "name": "Basic",
-        "currency": "£",
-        "price": 9,
         "items": [
-          {"productId": "KX4z2CZxzmUfSbiEsP30", "variantId": "0" , "qty": 2},
-          {"productId": "Z83Ya2tX7yNSUlCvysbD","variantId": "0", "qty": 1},
-          {"productId": "DDK5v1KHzawISSP3ZZqA", "variantId": "0", "qty": 1}
+          {"productId": "KX4z2CZxzmUfSbiEsP30", "variantSKU": "CKHC002-50", "qty": 2, "productInfos": {}},
+          {"productId": "Z83Ya2tX7yNSUlCvysbD", "variantSKU": "CKPB-MIX3", "qty": 1, "productInfos": {}},
+          {"productId": "DDK5v1KHzawISSP3ZZqA", "variantSKU": "CKACB0013", "qty": 1, "productInfos": {}}
         ],
-        "sku": "B-CH-001-01"
+        "sku": "B-CH-001-01",
+        "boxPrice": null,
+        "minBoxPrice": null,
+        "currency": "£"
       },
       {
         "name": "Premium",
-        "currency": "£",
-        "price": 14,
         "items": [
-          {"productId": "KX4z2CZxzmUfSbiEsP30", "variantId": "0" , "qty": 3},
-          {"productId": "Z83Ya2tX7yNSUlCvysbD","variantId": "0", "qty": 2},
-          {"productId": "DDK5v1KHzawISSP3ZZqA", "variantId": "0", "qty": 1}
+          {"productId": "KX4z2CZxzmUfSbiEsP30", "variantSKU": "CKHC002-50", "qty": 3, "productInfos": {}},
+          {"productId": "Z83Ya2tX7yNSUlCvysbD", "variantSKU": "CKPB-MIX3", "qty": 2, "productInfos": {}},
+          {"productId": "DDK5v1KHzawISSP3ZZqA", "variantSKU": "CKACB0013", "qty": 1, "productInfos": {}}
         ],
-        "sku": "B-CH-001-02"
+        "sku": "B-CH-001-02",
+        "boxPrice": null,
+        "minBoxPrice": null,
+        "currency": "£"
       }
     ]
   }
-]
-;
+];
 
 export default seedBoxes;
