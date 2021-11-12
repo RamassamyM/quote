97923891c9219672a4cbd89bc218b80281798f99
Ymassamar <ramassamymichael@gmail.com>
2021-11-12 16:50:58 +0100

    update Json with boxIdeas

--- src/seeder/seed-boxes.js
+++ src/seeder/seed-boxes.js
@@ -1,18 +1,19 @@
 const seedBoxes = [
   {
-    "title": "Christmas Box",
+    "title": "Christmas Gift Box",
     "category": "Themed",
     "description": "Give your employee a beautiful Christmas Box content for well-being with premium products like : 1 box of organic Chocolate, 1 body lotion...",
     "main_picture_url": "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/boxIdea_placeholder.png?alt=media&token=b85ed1ec-594c-4384-9e13-c53bc2576de9",
     "picture_urls": ["https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/boxIdea_placeholder.png?alt=media&token=b85ed1ec-594c-4384-9e13-c53bc2576de9", "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/45%20Degree%20GF%2Fckhandsan.png?alt=media&token=7a40d86a-9c89-4dba-9004-ce7eaa32d777"],
-    "tags": ["christmas", "gift", "food"],
+    "tags": ["christmas", "gift", "new year"],
     "variants": [
       {
-        "name": "Basic",
+        "name": "Standard",
         "items": [
-          {"productId": "I1oaY4hna7WJPTd5eyFl", "variantSKU": "CKPFS04", "qty": 3, "productInfos": {}},
-          {"productId": "J7xm2RJInDES9EB3po5i", "variantSKU": "CKTEA002-100", "qty": 1, "productInfos": {}},
-          {"productId": "KXYLvvhXJOqNsM1vH6uA", "variantSKU": "CKACB007", "qty": 1, "productInfos": {}}
+          {"productId": "vID9lq7tSOLSzI9d3o7K", "variantSKU": "CKMB001-100", "qty": 1, "productInfos": {}},
+          {"productId": "rcB615fevw3zUULuIRvN", "variantSKU": "CKPFS05", "qty": 1, "productInfos": {}},
+          {"productId": "AE1InJTaZoejwcnzU6lc", "variantSKU": "CKPC02", "qty": 1, "productInfos": {}},
+          {"productId": "TQZEXPn9W9dsWlEpR9aJ", "variantSKU": "CKTEA004-20", "qty": 1, "productInfos": {}}
         ],
         "sku": "B-CH-001-01",
         "boxPrice": null,
@@ -22,9 +23,15 @@ const seedBoxes = [
       {
         "name": "Premium",
         "items": [
-          {"productId": "I1oaY4hna7WJPTd5eyFl", "variantSKU": "CKPFS04", "qty": 4, "productInfos": {}},
-          {"productId": "J7xm2RJInDES9EB3po5i", "variantSKU": "CKTEA002-100", "qty": 2, "productInfos": {}},
-          {"productId": "KXYLvvhXJOqNsM1vH6uA", "variantSKU": "CKACB007", "qty": 1, "productInfos": {}}
+          {"productId": "vID9lq7tSOLSzI9d3o7K", "variantSKU": "CKMB001-100", "qty": 1, "productInfos": {}},
+          {"productId": "rcB615fevw3zUULuIRvN", "variantSKU": "CKPFS05", "qty": 1, "productInfos": {}},
+          {"productId": "O3bvmC4K3ShYnwxRgXoi", "variantSKU": "CKNB02", "qty": 1, "productInfos": {}},
+          {"productId": "TQZEXPn9W9dsWlEpR9aJ", "variantSKU": "CKTEA004-100", "qty": 1, "productInfos": {}},
+          {"productId": "4lptuqFFbEEomknVTOCS", "variantSKU": "CKMB002-100", "qty": 1, "productInfos": {}},
+          {"productId": "YdJLXEOdNc4KjDJF7JPI", "variantSKU": "CKACB0014", "qty": 1, "productInfos": {}},
+          {"productId": "2HJfFOWDGv7xb7ihbhR4", "variantSKU": "CKRS001-50", "qty": 1, "productInfos": {}},
+          {"productId": "l02TpFLgj8cxz8YVGgIC", "variantSKU": "CKHC002-50", "qty": 1, "productInfos": {}},
+          {"productId": "TQZEXPn9W9dsWlEpR9aJ", "variantSKU": "CKTEA004-100", "qty": 1, "productInfos": {}}
         ],
         "sku": "B-CH-001-02",
         "boxPrice": null,
@@ -34,21 +41,22 @@ const seedBoxes = [
     ]
   },
   {
-    "title": "Best Seller Box",
-    "category": "Occasions",
+    "title": "Rest & Sleep",
+    "category": "Themed",
     "description": "Reward your best sales employee with this incredible Box including well-being premium products like : 1 box of organic Chocolate, 1 body lotion...",
     "main_picture_url": "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/boxIdea_placeholder.png?alt=media&token=b85ed1ec-594c-4384-9e13-c53bc2576de9",
     "picture_urls": ["https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/boxIdea_placeholder.png?alt=media&token=b85ed1ec-594c-4384-9e13-c53bc2576de9", "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/45%20Degree%20GF%2Fckhandsan.png?alt=media&token=7a40d86a-9c89-4dba-9004-ce7eaa32d777"],
-    "tags": ["reward", "gift", "sales"],
+    "tags": ["Sleep", "Unwind", "Calm"],
     "variants": [
       {
-        "name": "Basic",
+        "name": "Standard",
         "items": [
-          {"productId": "I1oaY4hna7WJPTd5eyFl", "variantSKU": "CKPFS04", "qty": 2, "productInfos": {}},
-          {"productId": "J7xm2RJInDES9EB3po5i", "variantSKU": "CKTEA002-100", "qty": 1, "productInfos": {}},
+          {"productId": "4lptuqFFbEEomknVTOCS", "variantSKU": "CKMB002-100", "qty": 1, "productInfos": {}},
+          {"productId": "vFcqisKe7HVEedzCLAvZ", "variantSKU": "CKNB03", "qty": 1, "productInfos": {}},
+          {"productId": "N2huB2Nnir7YTgu5rETd", "variantSKU": "CKTEA003-20", "qty": 1, "productInfos": {}},
           {"productId": "KXYLvvhXJOqNsM1vH6uA", "variantSKU": "CKACB007", "qty": 1, "productInfos": {}}
         ],
-        "sku": "B-CH-001-01",
+        "sku": "B-RS-001-01",
         "boxPrice": null,
         "minBoxPrice": null,
         "currency": "£"
@@ -56,17 +64,100 @@ const seedBoxes = [
       {
         "name": "Premium",
         "items": [
-          {"productId": "I1oaY4hna7WJPTd5eyFl", "variantSKU": "CKPFS04", "qty": 3, "productInfos": {}},
-          {"productId": "J7xm2RJInDES9EB3po5i", "variantSKU": "CKTEA002-100", "qty": 2, "productInfos": {}},
-          {"productId": "KXYLvvhXJOqNsM1vH6uA", "variantSKU": "CKACB007", "qty": 1, "productInfos": {}}
+          {"productId": "4lptuqFFbEEomknVTOCS", "variantSKU": "CKMB002-100", "qty": 1, "productInfos": {}},
+          {"productId": "vFcqisKe7HVEedzCLAvZ", "variantSKU": "CKNB03", "qty": 1, "productInfos": {}},
+          {"productId": "N2huB2Nnir7YTgu5rETd", "variantSKU": "CKTEA003-20", "qty": 1, "productInfos": {}},
+          {"productId": "KXYLvvhXJOqNsM1vH6uA", "variantSKU": "CKACB007", "qty": 1, "productInfos": {}},
+          {"productId": "tdRcOjA1ptBnGuNIyCmj", "variantSKU": "CKPC01", "qty": 1, "productInfos": {}},
+          {"productId": "UOKjqJoIt2ZUu2OKRvO5", "variantSKU": "CKACB006", "qty": 1, "productInfos": {}}
         ],
-        "sku": "B-CH-001-02",
+        "sku": "B-RS-001-02",
+        "boxPrice": null,
+        "minBoxPrice": null,
+        "currency": "£"
+      }
+    ]
+  },
+  {
+    "title": "Mindfulness",
+    "category": "Themed",
+    "description": "Reward your best sales employee with this incredible Box including well-being premium products like : 1 box of organic Chocolate, 1 body lotion...",
+    "main_picture_url": "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/boxIdea_placeholder.png?alt=media&token=b85ed1ec-594c-4384-9e13-c53bc2576de9",
+    "picture_urls": ["https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/boxIdea_placeholder.png?alt=media&token=b85ed1ec-594c-4384-9e13-c53bc2576de9", "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/45%20Degree%20GF%2Fckhandsan.png?alt=media&token=7a40d86a-9c89-4dba-9004-ce7eaa32d777"],
+    "tags": ["Mental Health", "gift", "Unwind"],
+    "variants": [
+      {
+        "name": "Standard",
+        "items": [
+          {"productId": "zXsCxsvNCF8Vg0nyi40k", "variantSKU": "CKPC03", "qty": 1, "productInfos": {}},
+          {"productId": "rzLmJhDnh0pZsvkEy4xq", "variantSKU": "CKRS002-50", "qty": 1, "productInfos": {}},
+          {"productId": "YiJm3cl1ZUnBrLTHEEaI", "variantSKU": "CKNB-01", "qty": 1, "productInfos": {}},
+          {"productId": "gGrm0WBRMaC3pxkbYmpO", "variantSKU": "CKTEA006-20", "qty": 1, "productInfos": {}}
+        ],
+        "sku": "B-MF-001-01",
+        "boxPrice": null,
+        "minBoxPrice": null,
+        "currency": "£"
+      },
+      {
+        "name": "Premium",
+        "items": [
+          {"productId": "zXsCxsvNCF8Vg0nyi40k", "variantSKU": "CKPC03", "qty": 1, "productInfos": {}},
+          {"productId": "rzLmJhDnh0pZsvkEy4xq", "variantSKU": "CKRS002-50", "qty": 1, "productInfos": {}},
+          {"productId": "YiJm3cl1ZUnBrLTHEEaI", "variantSKU": "CKNB-01", "qty": 1, "productInfos": {}},
+          {"productId": "gGrm0WBRMaC3pxkbYmpO", "variantSKU": "CKTEA006-20", "qty": 1, "productInfos": {}},
+          {"productId": "77Qs7DG0wHDUCuR1qFbs", "variantSKU": "CKACB0011", "qty": 1, "productInfos": {}},
+          {"productId": "ZH4DXLxuzXwO5nPCg3MZ", "variantSKU": "CKOR001-10", "qty": 1, "productInfos": {}}
+        ],
+        "sku": "B-MF-001-02",
+        "boxPrice": null,
+        "minBoxPrice": null,
+        "currency": "£"
+      }
+    ]
+  },
+  {
+    "title": "Tranquility",
+    "category": "Themed",
+    "description": "Reward your best sales employee with this incredible Box including well-being premium products like : 1 box of organic Chocolate, 1 body lotion...",
+    "main_picture_url": "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/boxIdea_placeholder.png?alt=media&token=b85ed1ec-594c-4384-9e13-c53bc2576de9",
+    "picture_urls": ["https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/boxIdea_placeholder.png?alt=media&token=b85ed1ec-594c-4384-9e13-c53bc2576de9", "https://firebasestorage.googleapis.com/v0/b/curakit-7e00d.appspot.com/o/45%20Degree%20GF%2Fckhandsan.png?alt=media&token=7a40d86a-9c89-4dba-9004-ce7eaa32d777"],
+    "tags": ["Relax", "mindfulness", "Wellbeing"],
+    "variants": [
+      {
+        "name": "Standard",
+        "items": [
+          {"productId": "fRRSyZGEOoyWW08nyRiU", "variantSKU": "CKNB09", "qty": 1, "productInfos": {}},
+          {"productId": "AE1InJTaZoejwcnzU6lc", "variantSKU": "CKPC02", "qty": 1, "productInfos": {}},
+          {"productId": "Hy54MsVq8f5W8MxojiyL", "variantSKU": "CKPFS06", "qty": 1, "productInfos": {}},
+          {"productId": "BeTguxqbIwRqhS3YC402", "variantSKU": "CKACB004", "qty": 1, "productInfos": {}},
+          {"productId": "ZH4DXLxuzXwO5nPCg3MZ", "variantSKU": "CKOR001-10", "qty": 1, "productInfos": {}}
+        ],
+        "sku": "B-TQ-001-01",
+        "boxPrice": null,
+        "minBoxPrice": null,
+        "currency": "£"
+      },
+      {
+        "name": "Premium",
+        "items": [
+          {"productId": "fRRSyZGEOoyWW08nyRiU", "variantSKU": "CKNB09", "qty": 1, "productInfos": {}},
+          {"productId": "AE1InJTaZoejwcnzU6lc", "variantSKU": "CKPC02", "qty": 1, "productInfos": {}},
+          {"productId": "Hy54MsVq8f5W8MxojiyL", "variantSKU": "CKPFS06", "qty": 1, "productInfos": {}},
+          {"productId": "BeTguxqbIwRqhS3YC402", "variantSKU": "CKACB004", "qty": 1, "productInfos": {}},
+          {"productId": "ZH4DXLxuzXwO5nPCg3MZ", "variantSKU": "CKOR001-10", "qty": 1, "productInfos": {}},
+          {"productId": "N2huB2Nnir7YTgu5rETd", "variantSKU": "CKTEA003-100", "qty": 1, "productInfos": {}},
+          {"productId": "oneqjq1n6gBvoN2u32zf", "variantSKU": "CKNB04", "qty": 1, "productInfos": {}},
+          {"productId": "jTtFobLm1Y6GCUuAG0aI", "variantSKU": "CKHC001-50", "qty": 1, "productInfos": {}}
+        ],
+        "sku": "B-TQ-001-02",
         "boxPrice": null,
         "minBoxPrice": null,
         "currency": "£"
       }
     ]
   }
-];
+]
+;
 
 export default seedBoxes;
